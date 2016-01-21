47a2981fb03631011010883697102c65dafa0bf6
inka <inka.haltiapuu@gmail.com>
2016-01-21 03:54:48 +0200

    tidying up

--- image_share/server/startup.js
+++ image_share/server/startup.js
@@ -1,15 +1,13 @@
-if (Meteor.isServer){
-	Meteor.startup(function(){
-		//let's load our demo images if there are none in the db
-		if (Images.find().count() == 0){
-			for (var i=1; i<=18; i++){
-				Images.insert(
-					{
-	    			img_src:"img_"+i+".jpg",
-	    			img_alt:"image "+i
-					}
-				);
-			}//end of for insert images
-		}//end of if no images
-	});
-}
+Meteor.startup(function(){
+	//let's load our demo images if there are none in the db
+	if (Images.find().count() == 0){
+		for (var i=1; i<=18; i++){
+			Images.insert(
+				{
+    			img_src:"img_"+i+".jpg",
+    			img_alt:"image "+i
+				}
+			);
+		}//end of for insert images
+	}//end of if no images
+});
